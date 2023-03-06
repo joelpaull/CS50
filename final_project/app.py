eb1ff20905628f6682c8efc5670c526fe83ff202
Joel Paull <joelpaull@joels-mbp.home>
2023-03-06 17:06:21 +0000

    Created layout.html

--- final_project/app.py
+++ final_project/app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
 
+# Configure Flask
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("layout.html")

0de5d2424de1fbb985accf86f2240b01e6708a04
Joel Paull <joelpaull@joels-mbp.home>
2023-03-06 18:56:17 +0000

    Search button created

--- final_project/app.py
+++ final_project/app.py
@@ -1,10 +1,26 @@
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
+from pip._vendor import requests
+API_KEY = CtaQQ04zfxvU2PValOpLAvOL3jGwbnYx
+url = https://api.rsc.org/compounds/v1
 
 # Configure Flask
 app = Flask(__name__)
 
+def find_chem(chemical):
+    ''' Look up chemical against API'''
+    # Api Key
+    api_key = API_KEY
+    
+    
 
 @app.route("/")
 def index():
     return render_template("layout.html")
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    if request.method == "GET":
+        return render_template("search.html")
+    else:
+        chemical = request.form.get("chemical")
